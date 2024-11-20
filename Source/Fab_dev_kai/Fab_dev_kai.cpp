4be98c3e89839163e3b2095769db7af57ecb8d1a
johnny.cochrane <dev@johnco.org>
2024-11-20 14:57:25 -0800

    new: Source - init

---
+++ Source/Fab_dev_kai/Fab_dev_kai.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fab_dev_kai.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fab_dev_kai, "Fab_dev_kai" );
