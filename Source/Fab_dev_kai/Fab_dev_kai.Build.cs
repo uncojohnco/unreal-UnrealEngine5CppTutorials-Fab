cf8fca9900e8cf6165839117fb8f6fbee5cc8a64
johnny.cochrane <dev@johnco.org>
2024-11-20 18:05:05 -0800

    change: Build.cs - add `Fab_dev_kai` directory to `PublicIncludePaths`

--- Source/Fab_dev_kai/Fab_dev_kai.Build.cs
+++ Source/Fab_dev_kai/Fab_dev_kai.Build.cs
@@ -11,6 +11,8 @@ public class Fab_dev_kai : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.Add("Fab_dev_kai");
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
