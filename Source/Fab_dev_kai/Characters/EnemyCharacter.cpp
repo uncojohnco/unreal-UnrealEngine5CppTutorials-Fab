b279ecf5012d188de70e8d285e1050b562f0b758
johnny.cochrane <dev@johnco.org>
2024-11-21 19:11:58 -0800

    change: EnemyCharacter - implement AbilitySystemComponent

--- Source/Fab_dev_kai/Characters/EnemyCharacter.cpp
+++ Source/Fab_dev_kai/Characters/EnemyCharacter.cpp
@@ -3,18 +3,28 @@
 
 #include "EnemyCharacter.h"
 
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/FabAbilitySystemComponent.h"
+
 
 // Sets default values
 AEnemyCharacter::AEnemyCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UFabAbilitySystemComponent>("AbilitySystemComponent");
+	// Gameplay effects are not replicated to other AI controlled pawns
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
 }
 
 // Called when the game starts or when spawned
 void AEnemyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Setup enemy's AbilitySystemComponent
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 }
 
 // Called every frame
